3f245af1d12ffa76ce4e02cb6d769788034ac8c4
Ekaterina Kalacheva <ekkalacheva@gmail.com>
2024-12-11 16:04:34 +0300

    Game card click init system added.

---
+++ Assets/Scripts/GamePlay/GameCard/Components/ClickDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace MemoryGame.GamePlay
+{
+    internal class ClickDetector: MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            var colliders = GetComponentsInChildren<Collider2D>();
+
+            if (colliders.Length <= 0)
+            {
+                throw new Exception($"There are no any Colliders to handle click event on {name}");
+            }
+        }
+
+        private void OnMouseDown()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/GameCard/Systems/GameCardClickInitSystem.cs
@@ -0,0 +1,31 @@
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using Unity.IL2CPP.CompilerServices;
+using Scellecs.Morpeh;
+
+namespace MemoryGame.GamePlay
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(GameCardClickInitSystem))]
+    public sealed class GameCardClickInitSystem : UpdateSystem
+    {
+        private Filter _filter;
+
+        public override void OnAwake()
+        {
+            _filter = World.Filter.With<GameCard>().Without<Clickable>().Build();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var cardEntity in _filter)
+            {
+                var view = cardEntity.GetComponent<GameCard>();
+                var clickDetector = view.Transform.gameObject.AddComponent<ClickDetector>();
+                cardEntity.AddComponent<Clickable>();
+            }
+        }
+    }
+}
